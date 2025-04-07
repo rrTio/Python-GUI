d6a7a71db43dd8fe61cd3b7f83e2c8ff6de6880d
rrTio <rozsraventio523@gmail.com>
2025-04-08 00:20:11 +0800

    Added new window a layout and functions

--- login.py
+++ login.py
@@ -4,6 +4,7 @@ import tkinter.font as tkFont
 from tkinter import messagebox
 from tkinter import *
 import os
+import functions as func
 
 username = os.getenv("my_username")
 password = os.getenv("my_password")
@@ -18,6 +19,12 @@ def checker(event = None):
         os.system(open)
     else:
         messagebox.showerror("LOGIN", "INVALID ACCOUNT")
+
+def opener(event = None):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    open = f'python {script_dir}\\main.py'
+    root.destroy()
+    os.system(open)
 
 width = 320
 height = 200
@@ -36,10 +43,12 @@ root.geometry(dimension)
 root.title("Login")
 root.resizable(0, 0)
 
-labelframe = tk.Frame(root, bd=5, relief="solid")
+labelframe = tk.Frame(root, relief="solid")
 labelframe.pack(fill="x", expand=True)
 
-mainframe = tk.Frame(root, bd=1, relief="solid")
+labelframe.columnconfigure(0, weight=1)
+
+mainframe = tk.Frame(root)
 mainframe.pack(fill="both", expand=True, padx=5, pady=5)
 
 mainframe.columnconfigure(0, weight=2)
@@ -47,36 +56,32 @@ mainframe.columnconfigure(1, weight=3)
 mainframe.columnconfigure(2, weight=3)
 mainframe.columnconfigure(3, weight=2)
 
+mainframe.rowconfigure(0, weight=2)
 mainframe.rowconfigure(1, weight=2)
-mainframe.rowconfigure(2, weight=2)
-mainframe.rowconfigure(3, weight=3)
+mainframe.rowconfigure(2, weight=1)
+mainframe.rowconfigure(3, weight=1)
 mainframe.rowconfigure(4, weight=1)
-mainframe.rowconfigure(5, weight=1)
-mainframe.rowconfigure(6, weight=2)
 
-fontObj = tkFont.Font(size=15, weight="bold")
+fontObj = tkFont.Font(size=20, weight="bold")
 labelfont = tkFont.Font(size=10, weight="bold")
 reliefValue = "sunken"
 labelReliefValue = "solid"
 
-tk.Label(labelframe, text="USER LOGIN", fg="green", bg="black", font=fontObj).pack(fill="x")
-
-tk.Label(mainframe).grid(row=0, columnspan=4, sticky="nsew") #SPACER
+tk.Label(labelframe, text="USER LOGIN", fg="green", bg="black", font=fontObj).grid(row=0, column=0, sticky="sew")
 
 #USERNAME
 txtUSERNAME = StringVar()
-tk.Label(mainframe, text="Username", fg="green", font=labelfont).grid(row=1, column=1, sticky="nsew", pady=5)
-tk.Entry(mainframe, bd=2, relief=reliefValue, textvariable=txtUSERNAME).grid(row=1, column=2, sticky="nsew", pady=5)
+tk.Label(mainframe, text="Username", fg="green", font=labelfont).grid(row=0, column=1, sticky="nsew", pady=5)
+tk.Entry(mainframe, bd=2, relief=reliefValue, textvariable=txtUSERNAME).grid(row=0, column=2, sticky="nsew", pady=5)
 
 #PASSWORD
 txtPASSWORD = StringVar()
-tk.Label(mainframe, text="Password", fg="green", font=labelfont).grid(row=2, column=1, sticky="nsew")
-tk.Entry(mainframe, bd=2, relief=reliefValue, show = "*", textvariable=txtPASSWORD).grid(row=2, column=2, sticky="nsew")
+tk.Label(mainframe, text="Password", fg="green", font=labelfont).grid(row=1, column=1, sticky="nsew")
+tk.Entry(mainframe, bd=2, relief=reliefValue, show = "*", textvariable=txtPASSWORD).grid(row=1, column=2, sticky="nsew")
 
-tk.Label(mainframe).grid(row=3, column=0, columnspan=4, sticky="ew") #SPACER
+tk.Button(mainframe, text="Login", fg="green", bd=2, relief="raised", font=labelfont, command=checker).grid(row=3, column=1, columnspan=2, sticky="nsew", pady=5)
 
-tk.Button(mainframe, text="Login", fg="green", bd=1, relief="ridge", font=labelfont, command=checker).grid(row=4, column=1, columnspan=2, sticky="nsew")
-
-tk.Label(mainframe).grid(row=5, column=0, columnspan=4, sticky="ew") #SPACER
-
+signup = tk.Label(mainframe, text="Sign Up", fg="black", cursor="hand2")
+signup.grid(row=4, column=1, columnspan=2, sticky="nsew")
+signup.bind("<Button-1>", opener)
 root.mainloop()
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import os
 
 import functions as func
 
+global items, i
+items = []
+i = 0
+
 width = 600
 height = 350
 
@@ -26,6 +30,31 @@ def closeApp():
         root.destroy()
         func.logout()
 
+def addToList():
+    global i
+    i += 1
+    items.append(f"List item no. {i}")
+    listbox.delete(0, tk.END)
+    for item in items:
+        listbox.insert(tk.END, item)
+
+def removeToList():
+    if items:
+        items.pop()
+    listbox.delete(0, tk.END)
+    for item in items:
+        listbox.insert(tk.END, item)
+
+def removeSelected():
+    selectedItems = listbox.curselection()
+    for index in reversed(selectedItems):
+        del items[index]
+    listbox.delete(0, tk.END)
+    for item in items:
+        listbox.insert(tk.END, item)
+
+
+
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
@@ -40,10 +69,36 @@ root.resizable(0, 0)
 
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
+editmenu = Menu(menubar, tearoff=0)
+
 filemenu.add_command(label="New", command=openNew)
 filemenu.add_command(label="Open", command=func.nothing)
 filemenu.add_command(label="Exit", command=closeApp)
-menubar.add_cascade(label="File", menu=filemenu)
 
+editmenu.add_command(label="Add", command=func.nothing)
+
+menubar.add_cascade(label="File", menu=filemenu)
+menubar.add_cascade(label="Edit", menu=editmenu)
 root.config(menu=menubar)
+
+root.columnconfigure(0, weight=3)
+root.columnconfigure(1, weight=1)
+
+fontListText = tkFont.Font(size=12, weight="bold")
+
+listbox = tk.Listbox(root, fg="green", bg="black", justify="center", font=fontListText, selectmode="multiple")
+listbox.grid(row=0, column=0, sticky="ew")
+
+btnframe = tk.Frame(root)
+btnframe.grid(row=0, column=1, sticky="nsew", pady=2)
+
+btnframe.columnconfigure(0, weight=1)
+btnframe.columnconfigure(1, weight=2)
+btnframe.columnconfigure(2, weight=1)
+
+tk.Button(btnframe, text="Add", command=addToList).grid(row=0, column=1, sticky="ew", pady=2)
+tk.Button(btnframe, text="Remove", command=removeToList).grid(row=1, column=1, sticky="ew", pady=2)
+tk.Button(btnframe, text="Delete Selected Items", command=removeSelected).grid(row=2, column=1, sticky="ew", pady=2)
+
+
 root.mainloop()
